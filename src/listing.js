e83dfd37452a4b03715749a3e6128e413f096e3a
Duknam Yoo <yoo2001818@gmail.com>
2017-05-07 15:14:28 +0900

    Add file list in playback

--- src/listing.js
+++ src/listing.js
@@ -40,6 +40,52 @@ function readdir(readPath) {
     }))));
 }
 
+function processDir(pathVal) {
+  let listingPath = path.join(pathVal);
+  let realPath = path.join(config.video, pathVal);
+  let pathSliced = listingPath.split('/');
+  if (pathSliced[pathSliced.length - 1] === '') pathSliced.pop();
+  // Show the filesystem listing
+  return Promise.all([readdir(realPath)].concat(config.videoEncode ? [
+    readdir(path.join(config.videoEncode, pathVal))
+    // Ignore errors
+    .then(v => v, () => [])
+  ] : [Promise.resolve([])]))
+  .then(([list, encodeList]) => {
+    // For directories, we can just show it without any problem.
+    // However, for files, we should only allow mp4 files, without extension.
+    let directories = list.filter(stats => stats.isDirectory())
+      // Hide video encode directory
+      .filter(stats => path.resolve(realPath, stats.filename) !==
+        config.videoEncode)
+      .map(stats => ({
+        filename: stats.filename,
+        path: encodeurl(path.resolve(listingPath,
+          encodeURIComponent(stats.filename))),
+        updated: stats.mtime,
+      }));
+    let files = list.filter(stats => stats.isFile() &&
+      /\.(mp4|mkv|m4v)$/.test(stats.filename)).map(stats => ({
+        filename: stats.filename.slice(0, -4),
+        path: encodeurl(path.resolve(listingPath,
+          encodeURIComponent(stats.filename.slice(0, -4)))),
+        size: stringifySize(stats.size),
+        updated: stats.mtime,
+        hasSubtitle:
+          list.some(v => v.filename.startsWith(stats.filename.slice(0, -4))
+            && v.filename.endsWith('.srt')),
+        encodes: encodeList
+          .filter(v => v.filename.startsWith(stats.filename.slice(0, -4))
+            && /\.(mp4|mkv|m4v)$/.test(v.filename))
+          .map(v => ({
+            name: /\.([^.]+)\.(mp4|mkv|m4v)$/.exec(v.filename)[1],
+            size: stringifySize(v.size),
+          })),
+      }));
+    return { root: listingPath === '/', directories, files };
+  });
+}
+
 module.exports = function listing(req, res, next) {
   if (req.method !== 'GET' && req.method !== 'HEAD') return next();
   let pathVal = decode(parseurl(req).pathname);
@@ -53,11 +99,14 @@ module.exports = function listing(req, res, next) {
   if (pathSliced[pathSliced.length - 1] === '') pathSliced.pop();
   let baseLocals = {
     path: listingPath,
+    pathEncoded: encodeurl(listingPath),
     breadcrumbs: pathSliced.map((v, i, arr) => ({
       name: v,
       sliced: encodeurl(pathSliced.slice(0, i + 1).join('/')) || '/',
     })),
-    ascendPath: encodeurl(path.join(listingPath, '..')),
+    ascendPath: path.join(listingPath, '..'),
+    ascendPathEncoded: encodeurl(path.join(listingPath, '..')),
+    formatDate,
   };
   fs.stat(realPath)
   .then(stats => {
@@ -69,47 +118,9 @@ module.exports = function listing(req, res, next) {
       return;
     }
     if (!stats.isDirectory()) return next();
-    // Show the filesystem listing
-    return Promise.all([readdir(realPath)].concat(config.videoEncode ? [
-      readdir(path.join(config.videoEncode, pathVal))
-      // Ignore errors
-      .then(v => v, () => [])
-    ] : [Promise.resolve([])]))
-    .then(([list, encodeList]) => {
-      // For directories, we can just show it without any problem.
-      // However, for files, we should only allow mp4 files, without extension.
-      let directories = list.filter(stats => stats.isDirectory())
-        // Hide video encode directory
-        .filter(stats => path.resolve(realPath, stats.filename) !==
-          config.videoEncode)
-        .map(stats => ({
-          filename: stats.filename,
-          path: encodeurl(path.resolve(listingPath,
-            encodeURIComponent(stats.filename))),
-          updated: stats.mtime,
-        }));
-      let files = list.filter(stats => stats.isFile() &&
-        /\.(mp4|mkv|m4v)$/.test(stats.filename)).map(stats => ({
-          filename: stats.filename.slice(0, -4),
-          path: encodeurl(path.resolve(listingPath,
-            encodeURIComponent(stats.filename.slice(0, -4)))),
-          size: stringifySize(stats.size),
-          updated: stats.mtime,
-          hasSubtitle:
-            list.some(v => v.filename.startsWith(stats.filename.slice(0, -4))
-              && v.filename.endsWith('.srt')),
-          encodes: encodeList
-            .filter(v => v.filename.startsWith(stats.filename.slice(0, -4))
-              && /\.(mp4|mkv|m4v)$/.test(v.filename))
-            .map(v => ({
-              name: /\.([^.]+)\.(mp4|mkv|m4v)$/.exec(v.filename)[1],
-              size: stringifySize(v.size),
-            })),
-        }));
-      // I don't feel good for including formatting function inside here,
-      // but there's no other way.
-      res.render('listing', Object.assign({ root: listingPath === '/',
-       directories, files, formatDate }, baseLocals));
+    return processDir(pathVal)
+    .then(v => {
+      res.render('listing', Object.assign(v, baseLocals));
     });
   }, err => {
     if (err.code !== 'ENOENT') throw err;
@@ -120,14 +131,17 @@ module.exports = function listing(req, res, next) {
     let parentPath = path.join(realPath, '..');
     let remainingPath = realPath.slice(parentPath.length + 1);
     let parentListingPath = path.join(listingPath, '..');
-    return Promise.all([readdir(parentPath)].concat(config.videoEncode ? [
-      readdir(parentEncodePath)
-      // Ignore errors
-      .then(v => v, () => [])
-    ] : [Promise.resolve([])]))
-    .then(lists => lists.map(list => list.filter(v => v.isFile() &&
-      v.filename.startsWith(remainingPath))))
-    .then(([list, encodeList]) => {
+    return Promise.all([
+      Promise.all([readdir(parentPath)].concat(config.videoEncode ? [
+        readdir(parentEncodePath)
+        // Ignore errors
+        .then(v => v, () => [])
+      ] : [Promise.resolve([])]))
+      .then(lists => lists.map(list => list.filter(v => v.isFile() &&
+        v.filename.startsWith(remainingPath)))),
+      processDir(parentListingPath)
+    ])
+    .then(([[list, encodeList], dirInfo]) => {
       if (list.length === 0) return next();
       // Look for mp4 file
       let mp4File = list.find(v => /\.(mp4|mkv|m4v)$/.test(v.filename));
@@ -164,9 +178,12 @@ module.exports = function listing(req, res, next) {
       let vttFiles = srtFiles.map(v => Object.assign({}, v, {
         path: v.path.slice(0, -4) + '.vtt'
       }));
+      dirInfo.files = dirInfo.files.map(v => Object.assign({}, v, {
+        selected: v.path === baseLocals.pathEncoded,
+      }));
       // Render it....
       res.render('playback', Object.assign({ name: remainingPath, mp4Files,
-        srtFiles, vttFiles }, baseLocals));
+        srtFiles, vttFiles, listing: dirInfo }, baseLocals));
     });
   })
   .catch(err => {
